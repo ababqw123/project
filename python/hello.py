d855c055cca6aa0868ff98ae93a8398397e0c596
Jay <ababqw123@gmail.com>
2021-12-24 14:51:02 +0900

    practice

--- python/hello.py
+++ python/hello.py
@@ -1,4 +1,65 @@
-station = "사당", "신도림", "인천공항"
+# station = "사당", "신도림", "인천공항"
 
-for(a=0,2,1)
-print(station[a] + " 행 열차가 들어오고 있습니다.")
+# for(a=0,2,1)
+# print(station[a] + " 행 열차가 들어오고 있습니다.")
+
+# from math import *
+# print(floor(4.99))
+# print(ceil(3.1))
+
+# from random import *
+
+# print(int(random()*10))
+
+# from random import *
+
+# date = randint(4,28)
+
+# print("오프라인 스터디 모임 날자는 매월 " + str(date) + "일로 선정되었습니다.")
+
+# glga = "Python is Amazing"
+
+# # print(glga)
+# # print(glga.upper())
+# # print(glga.lower())
+# # print(len(glga))
+# # print(glga.islower())
+# # print(glga.replace("Python", "Java"))
+
+# index = glga.index("n")
+# print(index)
+
+# print("나는 %d살입니다." % 10)
+# print("나는 {}살입니다." .format(10))
+# print("나는 {1}색과 {0}색을 좋아해요." .format("빨강","초록"))
+
+# age = 20
+# color = "빨강색"
+# print(f"나는 {age}살이고, {color}색을 좋아합니다.")
+
+# site = "http://naver.com"
+# a = site.replace("http://","")
+# b = a[:a.index(".")]
+# c = b[:3]
+# print(c + str(len(b)) + str(b.count("e")) + "!")
+
+# subway = [10, 20, 30]
+
+# print(subway[0])
+
+# subway1 = ["재영", "성창", "성만"]
+
+# subway1.append("주현")
+# print(subway1)
+# subway1.insert(1,"수현")
+# print(subway1)
+# subway1.pop()
+# print(subway1)
+
+list = [3,2,6,1,7,4]
+list.sort()
+print(list)
+list.reverse()
+print(list)
+list.clear()
+print(list)
