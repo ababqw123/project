eb4d9c6302211463c3f7db490010a87cfee20e8f
Jay <ababqw123@gmail.com>
2021-12-27 11:21:40 +0900

    practice

--- python/hello.py
+++ python/hello.py
@@ -56,10 +56,84 @@
 # subway1.pop()
 # print(subway1)
 
-list = [3,2,6,1,7,4]
-list.sort()
-print(list)
-list.reverse()
-print(list)
-list.clear()
-print(list)
+# list = [3,2,6,1,7,4]
+# list.sort()
+# print(list)
+# list.reverse()
+# print(list)
+# list.clear()
+# print(list)
+
+# cabinet = {3:"유재석", 100:"김태호"}
+# print(cabinet[3])
+# print(cabinet[100])
+# print(cabinet.get(3))
+# print(3 in cabinet)
+
+# bag = {"a-100":"안재영", "b-20":"노성창"}
+# bag["c-30"] = "김주현"
+
+# print(bag.keys())
+# print(bag.values())
+# print(bag.items())
+
+# my_set={1,2,3,3,3,}
+# print(my_set)
+
+# java = {"성창","주현","성만"}
+# python = set(["재영","수현","성창"])
+
+# print(java & python) #intersection
+# print(java | python) #union
+# print(java - python) # difference
+
+# menu = {"커피", "우유", "쥬스"}
+
+# print(menu,type(menu))
+# menu = tuple(menu)
+# print(menu,type(menu))
+# menu = list(menu)
+# print(menu,type(menu))
+# menu = set(menu)
+# print(menu,type(menu))
+
+# from random import *
+
+# lst = range(1,21)
+# print(type(lst))
+# lst = list(lst)
+# print(type(lst))
+
+# shuffle(lst)
+# print(lst)
+# win = sample(lst,4)
+# print(win)
+
+# print("-- 당첨자 발표 --")
+# print("치킨 당첨자 : {}" .format(win[0]))
+# print("커피 당첨자 : {}" .format(win[1:]))
+# print("-- 축하합니다 --")
+
+# weather = input("오늘 날씨는 어때요?")
+# if weather == "비" :
+#    print("우산을 챙기세요")
+# elif weather == "미세먼지" :
+#     print("마스크 챙기세요")
+# else :
+#     print("준비물 필요 없어요")
+
+# for wait_no in range(1,5) :
+#     print("대기번호 : {} " .format(wait_no))
+
+# cos = ["재영", "성창", "성만", "주현"]
+# for star in cos :
+#     print("{}, 커피가 준비되었습니다." .format(star))
+
+# cos = "재영"
+# index = 5
+# while index >= 1:
+#     print("{}님, 커피가 준비되었습니다. {}번 남았습니다." .format(cos,index))
+#     index -= 1
+#     if index == 0 :
+#         print("커피는 폐기처분 되었습니다.")
+    
